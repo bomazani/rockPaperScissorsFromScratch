889c8bca793cd16893a885499dceb79a953ab015
Bob Hunt <bob.hunt.email@gmail.com>
2018-10-09 15:41:12 -0400

    working on radio button functionality

--- script.js
+++ script.js
@@ -1,14 +1,12 @@
+let CapFirstLtr = (string) => string.charAt(0).toUpperCase() + string.slice(1);
+let randomNum=Math.floor(Math.random() * 3);
 
 let userChoice = prompt("Do you choose Rock, Paper, or Scissors?");
+let choice1 = CapFirstLtr(userChoice);
+
 let computerChoiceArray=["Rock","Paper","Scissors"];
-
-let randomNum=Math.floor(Math.random() * 3);
 let computerChoice = computerChoiceArray[randomNum];
-
-let choice1 = userChoice;
 let choice2 = computerChoice;
-console.log(choice1);
-console.log(choice2);
 
 function compare(choice1, choice2) {
     if (choice1 === choice2) {
@@ -37,5 +35,13 @@ function compare(choice1, choice2) {
     }
 }
 
-console.log(compare(choice1, choice2));
-document.write(compare(choice1,choice2));
+var node1 = document.createElement("div");
+let textnode1 = document.createTextNode("Do you choose Rock, Paper, or Scissors?");
+node1.appendChild(textnode1);
+document.getElementById("instrux").appendChild(textnode1);
+
+
+var node2 = document.createElement("div");
+let textnode2 = document.createTextNode(compare(choice1,choice2));
+node2.appendChild(textnode2);
+document.getElementById("results").appendChild(textnode2);
